a4d19455749b2cb2fe1f2fde19c1d5b7debb502a
Gary Rojas <naruto13581@gmail.com>
2026-04-23 16:39:35 -0400

    Initial project setup - Spring Boot with Security, JPA, H2, Thymeleaf

---
+++ src/main/java/com/gary/fraudflagger/FraudFlaggerApplication.java
@@ -0,0 +1,13 @@
+package com.gary.fraudflagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudFlaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudFlaggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gary/fraudflagger/FraudFlaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gary.fraudflagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudFlaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
